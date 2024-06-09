6cf657215bc98677f68185910efdfccdc2c3b857
Dandres24 <dasanchezmendez@correo.unicordoba.edu.co>
2024-06-09 15:07:51 -0500

    actualizacion mongo adb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
+
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 import { UsersModule } from './users/users.module';
@@ -8,6 +14,9 @@ import { LogsModule } from './logs/logs.module';
 
 
 @Module({
+  ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+ ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'client') }),
+ MongooseModule.forRoot(process.env.DB_URI),
   imports: [EventsModule, UsersModule, TicketModule, LogsModule],
   controllers: [AppController],
   providers: [AppService],
