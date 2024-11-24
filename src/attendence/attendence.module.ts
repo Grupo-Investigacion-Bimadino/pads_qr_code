4104623a2e6e754307a8d43268325365efcc75a4
Ldavid182 <134534808+Ldavid182@users.noreply.github.com>
2024-11-24 20:59:52 +0000

    add ultimo en casa de karol

--- src/attendence/attendence.module.ts
+++ src/attendence/attendence.module.ts
@@ -3,6 +3,7 @@ import { AttendenceService } from './attendence.service';
 import { AttendenceController } from './attendence.controller';
 import { Attendence, AttendenceSchema } from './schemas/attendence.schemas';
 import { MongooseModule } from '@nestjs/mongoose';
+import { junit } from 'node:test/reporters';
 
 @Module({
   imports:[MongooseModule.forFeature([{name: Attendence.name, schema: AttendenceSchema }])], 
