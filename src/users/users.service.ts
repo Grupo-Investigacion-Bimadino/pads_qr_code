d6704e22cecb5a9dd86f42d0d87ace53689cc728
Ldavid182 <134534808+Ldavid182@users.noreply.github.com>
2024-11-29 19:46:44 +0000

    service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,63 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Users } from './schemas/users.schemas';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return createUserDto;
+  constructor(@InjectModel(Users.name) private usersModel: Model<Users>) {}
+
+  create(CreateUsersDto: CreateUserDto) {
+    const createdUsers = new this.usersModel(CreateUsersDto);
+    return createdUsers.save();
   }
 
   findAll() {
-    return [{ 
-      "Id": "1", 
-      "nombre de usuario": "pedrito2412", 
-      "rol": "docente", 
-      "identificacion": "113254367", 
-      "id_asistencia": "1",  
-      "id_eventos": "3" 
-     },
-     { 
-      "Id": "2", 
-      "nombre de usuario": "Dandres.24", 
-      "rol": "estudiante", 
-      "identificacion": "1005964827", 
-      "id_asistencia": "16",  
-      "id_eventos": "2" 
-     },
-     { 
-      "Id": "3", 
-      "nombre de usuario": "juan.252", 
-      "rol": "estudiante", 
-      "identificacion": "1004627932", 
-      "id_asistencia": "14",  
-      "id_eventos": "2" 
-     }];
+    return this.usersModel.find().exec();
   }
 
-  findOne(id: number) {
-    return { 
-      "Id": "2", 
-      "nombre de usuario": "Dandres.24", 
-      "rol": "estudiante", 
-      "identificacion": "1005964827", 
-      "id_asistencia": "16",  
-      "id_eventos": "2" 
-     };
+  findOne(id: string) {
+    return this.usersModel.findById(id).exec();
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return updateUserDto;
+  update(id: string, UpdateUsersDto: UpdateUserDto) {
+    return this.usersModel
+      .findByIdAndUpdate(id, UpdateUsersDto, {
+        new: true,
+      })
+      .exec();
   }
 
-  remove(id: number) {
-    return { 
-      "Id": "1", 
-      "nombre de usuario": "pedrito2412", 
-      "rol": "docente", 
-      "identificacion": "113254367", 
-      "id_asistencia": "1",  
-      "id_eventos": "3" 
-     };
+  remove(id: string) {
+    return this.usersModel.findByIdAndDelete(id).exec();
   }
 }
