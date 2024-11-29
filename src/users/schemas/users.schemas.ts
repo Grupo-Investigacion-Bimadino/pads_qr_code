2e7283e431de5980dad89165a3f925262709cdf8
Ldavid182 <134534808+Ldavid182@users.noreply.github.com>
2024-11-29 20:34:31 +0000

    last commit 2024

--- src/users/schemas/users.schemas.ts
+++ src/users/schemas/users.schemas.ts
@@ -1,8 +1,9 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+// Define la clase para el esquema de Users
 @Schema({ timestamps: true })
-export class Message extends Document {
+export class Users extends Document {
   @Prop()
   nombre: string;
 
@@ -13,4 +14,6 @@ export class Message extends Document {
   identificacion: string;
 }
 
-export const MessageSchema = SchemaFactory.createForClass(Message);
+// Genera el esquema de Mongoose a partir de la clase Users
+export const UsersSchema = SchemaFactory.createForClass(Users);
+
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,7 +3,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Users } from './schemas/users.schemas';
+import { Users } from './schemas/users.schemas'; // Importando la clase Users correctamente
 
 @Injectable()
 export class UsersService {
